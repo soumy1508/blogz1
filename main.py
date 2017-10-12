541a9b45d01dc78083c6e1dca650b1e9bda11b9a
soumya <soumy1508@gmail.com>
2017-10-11 22:59:56 -0500

    added oneblog

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, redirect, render_template, session, flash
 from flask_sqlalchemy import SQLAlchemy
+import cgi
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -19,27 +20,54 @@ class Blog(db.Model):
         
 
 
-@app.route('/', methods=['GET'])
+@app.route('/blog', methods=['GET'])
 def index(): 
     # we have to write code to get the data from the database
     # get the list of blogs from the table
     # give the list of blogs to the blog html page
+    blogid = request.args.get("id")
+    if blogid:
+        singleblog = Blog.query.filter_by(id=int(blogid)).first()
+        return render_template('oneblog.html', blog=singleblog)
+    else:
+        listofblogs = Blog.query.all()
+        return render_template('blog.html',blogs=listofblogs)
 
-    listofblogs = Blog.query.all()
-    return render_template('blog.html',blogs=listofblogs)
-
-
+    
 @app.route('/newpost', methods=['POST','GET'])
 def newpost():   
     if request.method == 'POST':
         user_title = request.form['title']
         user_body = request.form['body']
-        newBlog = Blog(user_title, user_body)
-        db.session.add(newBlog)
-        db.session.commit()
-        return redirect('/') 
+
+        # do validations here
+        # if validation Pass then store data in Db
+        # else redirect to newpost page with error messages
+                
+        titleerrormessage = ""
+        bodyerrormessage = ""
+        
+        if (not user_title) or (user_title.strip() == ""):
+            titleerrormessage = "Please enter title"
+
+        if (not user_body) or (user_body.strip() == ""):
+            bodyerrormessage = "Please enter body"
+        
+        if titleerrormessage or bodyerrormessage:
+            # go to page and display the error message
+            return redirect("/newpost?titleerror=" + titleerrormessage + "&bodyerror=" + bodyerrormessage)
+        else:
+            newBlog = Blog(user_title, user_body)
+            db.session.add(newBlog)
+            db.session.commit()
+            return redirect("/blog?id=" + str(newBlog.id))            
+            #return redirect('/blog')
     else:
-        return render_template('newpost.html')
+        titleerror = request.args.get("titleerror")
+        bodyerror = request.args.get("bodyerror")
+        return render_template('newpost.html',
+                                titleerror=titleerror and cgi.escape(titleerror, quote=True),
+                                bodyerror=bodyerror and cgi.escape(bodyerror, quote=True) )
 
 
 if __name__ == '__main__':
